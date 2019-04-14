d8890cc92538c044b785b0be0d9f6e200518e3ea
Semen <semenmsu@gmail.com>
2019-04-14 17:13:55 +0500

    add config

---
+++ bitstamp/config.py
@@ -0,0 +1,26 @@
+'''
+### CurrencyPairs
+btcusd btceur eurusd xrpusd xrpeur
+xrpbtc ltcusd ltceur ltcbtc ethusd 
+etheur ethbtc bchusd bcheur bchbtc
+
+### Channels
+- live_trades
+- live_orders
+- order_book
+- detail_order_book
+- diff_order_book
+'''
+
+config = {
+    "btcusd": ["live_trades"],
+    "btceur": ["live_trades"],
+    "eurusd": ["live_trades"],
+    "xrpusd": ["live_trades"],
+    "xrpeur": ["live_trades"],
+    "xrpbtc": ["live_trades"],
+    "ltcusd": ["live_trades"],
+    "ltceur": ["live_trades"],
+    "ltcbtc": ["live_trades"],
+    "ethusd": ["live_trades"]
+}
