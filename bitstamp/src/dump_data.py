86b48e810fc7188e461393178feeb5ee67f33eb7
Semen <semenmsu@gmail.com>
2019-04-16 16:26:12 +0500

    some ref

--- bitstamp/src/dump_data.py
+++ bitstamp/src/dump_data.py
@@ -16,19 +16,18 @@ if "APP_ENV" in os.environ:
 
 bts = Bitstamp(config)
 current_date = datetime.today().strftime('%Y%m%d')
-data_dump = open(f"{dump_dir}bitstamp-dump-{current_date}",
-                 'a+', buffering=bufsize)
+data_dump_file = open(f"{dump_dir}bitstamp-dump-{current_date}",
+                      'a+', buffering=bufsize)
 
 
 def on_exit():
-    data_dump.close()
+    data_dump_file.close()
     exit(0)
 
 
 def on_message(message):
-    s = datetime.utcnow().strftime('%Y%m%d %H:%M:%s')
-    print(s)
-    data_dump.write(str(int(time.time()*1000000))+","+str(message)+"\n")
+    #s = datetime.utcnow().strftime('%Y%m%d %H:%M:%s')
+    data_dump_file.write(str(int(time.time()*1000000))+","+str(message)+"\n")
 
 
 atexit.register(on_exit)
