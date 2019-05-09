2565f86d75df48193310ee168951aad73a4538eb
Semen <semenmsu@gmail.com>
2019-05-09 15:20:06 +0500

    add zmq_client for bitstamp

---
+++ bitstamp/src/scripts/zmq_client.py
@@ -0,0 +1,23 @@
+import zmq
+import time
+import json
+
+symbol = b'bitstamp@btcusd!'
+
+while True:
+    try:
+        context = zmq.Context()
+        receiver = context.socket(zmq.SUB)
+        receiver.connect("tcp://127.0.0.1:35001")
+        receiver.subscribe(symbol)
+        while True:
+            msg = receiver.recv_multipart()
+            body = json.loads(msg[1].decode("ascii"))
+            print(msg)
+    except Exception as err:
+        print("Exception: ", err)
+    finally:
+        del context
+        del receiver
+    print("[data stream] wait time for reconnecting")
+    time.sleep(5)
